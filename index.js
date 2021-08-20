6c26dbb615eb5e3efe1710949053982c4d3304f7
Christopher Campbell <christytc10@gmail.com>
2021-08-20 16:07:51 +0100

    Add basic league subscription

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const axios = require('axios');
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -9,8 +10,20 @@ client.on('ready', () => console.log('The Bot is ready!'));
 
 client.on('message', (msg) => {
   if (msg.content === '!ping') {
-    console.log(msg.channel);
     msg.channel.send("Pong");
+  } else if (msg.content.startsWith("!sleeper-subscribe")){
+    const parts = msg.content.split(" ");
+    if(parts.length != 2){
+      msg.channel.send("Usage: `!sleeper-subscribe 123` where 123 is the Sleeper league ID");
+      return;
+    }
+    const league_id = parts[1];
+    axios.get(`https://api.sleeper.app/v1/league/${league_id}`).then(res => {
+      msg.channel.send(`Subscribe to league ${res.data.name}`);
+    }).catch(err => {
+      msg.channel.send(`Couldn't subscribe to Sleeper league ${league_id}`);
+      return;
+    });
   }
 });
 
