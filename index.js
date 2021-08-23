86d4e077f7a1db715f0ee236bd9bb6171e5cde38
Christy Campbell <christytc10@gmail.com>
2021-08-23 13:02:29 +0100

    subscriptions save channel

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const Discord = require('discord.js');
+const { Client, MessageEmbed, WebhookClient } = require('discord.js');
 const axios = require('axios');
 const createClient = require('@supabase/supabase-js').createClient
+
 require('dotenv').config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
-const client = new Discord.Client();
+const client = new Client();
+const playersResponse = axios.get("https://api.sleeper.app/v1/players/nfl").then(r => { return r.data })
 
 client.login(process.env.BOT_TOKEN);
 client.on('ready', async () => {
     console.log('The Bot is ready!');
     setInterval(function(){ // repeat this every 5 minutes
         checkTransactions();
-    }, 300000 )
+    }, 5000 )
 });
 
 client.on('message', async (msg) => {
   if (msg.content === '!ping') {
+    console.log(msg.channel.id);
+    console.log(msg.guild.id);
     msg.channel.send("Pong");
   } else if (msg.content.startsWith("!sleeper-subscribe")){
     const parts = msg.content.split(" ");
@@ -25,49 +29,114 @@ client.on('message', async (msg) => {
     }
     const leagueId = parts[1];
     axios.get(`https://api.sleeper.app/v1/league/${leagueId}`).then( async res => {
-      saveSubscription(msg.channel.id, leagueId);
-      console.log(res.data.name);
-      console.log(res.data.avatar);
-      const webhook = await createWebhook(msg.channel, res.data.name + " Transactions", "https://sleepercdn.com/avatars/thumbs/" + res.data.avatar);
-      
+      saveSubscription(msg.channel.guild.id, msg.channel.id, leagueId);
       msg.channel.send(`Subscribed to league ${res.data.name}`);
     }).catch(err => {
+      console.log(err);
       msg.channel.send(`Couldn't subscribe to Sleeper league ${leagueId}`);
       return;
     });
   }
 });
 
-async function createWebhook(channel, hookName, hookAvatar) {
-    //TODO check if webhook exists - how?
-    //TODO store the webhook ID- do we need the channel too?
-    return await channel.createWebhook(hookName, {
-        avatar: hookAvatar,
-        })
-        .then(webhook => {
-            console.log(`Created webhook ${webhook}`);
-            return webhook;
-        })
-        .catch(console.error);
-        return undefined;
-};
-
 async function checkTransactions() {
-    //TODO - on schedule, run through the subscriptions and get transactions
-    //TODO - get the current game week (https://api.sleeper.app/v1/state/nfl)
     //TODO - send the latest transactions to the webhook
-    console.log("TEST");
-    const secondsSinceEpoch = Math.round(Date.now() / 1000)
-    console.log(secondsSinceEpoch);
+    const nflInfo = await axios.get('https://api.sleeper.app/v1/state/nfl')
+    const nflWeek = nflInfo.data.leg > 0 ? nflInfo.data.leg : 1;
+    const epochMillis = Math.round(Date.now())
+    
+    const subs = await supabase.from('sleeper-subs')
+        .select();
+    subs.data.forEach(async sub => {
+        const channel = await client.channels.cache.find(c => c.guild.id == sub.guild &&
+            c.type == 'text' &&
+            c.id == sub.channel);
+
+        const leagueRosters = await axios.get(`https://api.sleeper.app/v1/league/${sub.league_id}/rosters`).then(r => r.data);
+        const leagueUsers = await axios.get(`https://api.sleeper.app/v1/league/${sub.league_id}/users`).then(r => r.data);
+        
+        const txns = await axios.get(`https://api.sleeper.app/v1/league/${sub.league_id}/transactions/${nflWeek}`)
+        const newTxns = txns.data.filter(txn => txn.status == 'complete' && txn.status_updated > sub.latest);
+        newTxns.forEach(async txn => {
+            const players = await playersResponse
+            if(txn.type=='trade'){
+                const tradeMessage = buildTradeMessage(players, leagueRosters, leagueUsers, txn);
+                channel.send(tradeMessage);          
+            }
+            if(txn.type=='waiver' || txn.type=='free_agent'){
+                const pickupMessage = buildPickupMessage(players, leagueRosters, leagueUsers, txn);
+                channel.send(pickupMessage);          
+            }
+        })
+        //TODO - update latest txn time in DB here
+    });
 };
 
-async function saveSubscription(channelId, leagueId) {
-    const exists = await subscriptionExists(channelId, leagueId);
+function buildPickupMessage(players, leagueRosters, leagueUsers, txn){
+    const trade = {}
+    const stringParts = ["__**Free Agent Pickup**__"];
+    for (const [playerId, rosterId] of Object.entries(txn.adds)) {
+        const username = getUsernameForRosterId(leagueRosters, leagueUsers, rosterId);
+        if (!trade[username]){
+            trade[username] = []
+        }
+        trade[username].push(players[playerId].full_name);
+    }   
+    txn.draft_picks.forEach(pick => {
+        const username = getUsernameForRosterId(leagueRosters, leagueUsers, pick.owner_id);
+        if (!trade[username]){
+            trade[username] = []
+        }
+        trade[username].push(`${pick.season} ${pick.round}`);
+    })
+    for (const [username, tradeParts] of Object.entries(trade)) {
+        stringParts.push(`**${username}** adds`);
+        stringParts.push(tradeParts.join('\n'));
+    }
+    return stringParts.join("\n");
+}
+
+function buildTradeMessage(players, leagueRosters, leagueUsers, txn){
+    const trade = {}
+    for (const [playerId, rosterId] of Object.entries(txn.adds)) {
+        const username = getUsernameForRosterId(leagueRosters, leagueUsers, rosterId);
+        if (!trade[username]){
+            trade[username] = []
+        }
+        trade[username].push(players[playerId].full_name);
+    }   
+    txn.draft_picks.forEach(pick => {
+        const username = getUsernameForRosterId(leagueRosters, leagueUsers, pick.owner_id);
+        if (!trade[username]){
+            trade[username] = []
+        }
+        trade[username].push(`${pick.season} ${pick.round}`);
+    })
+    const stringParts = ["__**Trade**__"];
+    for (const [username, tradeParts] of Object.entries(trade)) {
+        stringParts.push(`**${username}** adds`);
+        stringParts.push(tradeParts.join('\n'));
+    }
+    return stringParts.join("\n");
+}
+
+function getUsernameForRosterId( rosters, users, rosterId){
+    const roster = rosters.find(r => {
+        return r.roster_id == rosterId;
+    });
+    const user = users.find(u => {
+        return u.user_id == roster.owner_id
+    });
+    return user.display_name;
+}
+
+async function saveSubscription(guildId, channelId, leagueId) {
+    const exists = await subscriptionExists(guildId, channelId, leagueId);
     if (!exists) {
-        const secondsSinceEpoch = Math.round(Date.now() / 1000)
+        const epochMillis = Math.round(Date.now())
         supabase.from('sleeper-subs')
             .insert([
-                { channel: channelId, league_id: leagueId, latest: secondsSinceEpoch }
+                { guild: guildId, channel: channelId, league_id: leagueId, latest: epochMillis }
             ]).then(dbRes => {
                 return true;
             }).catch(err => {
@@ -77,9 +146,10 @@ async function saveSubscription(channelId, leagueId) {
     }
 };
 
-async function subscriptionExists(channelId, leagueId) {
+async function subscriptionExists(guildId, channelId, leagueId) {
     return await supabase.from('sleeper-subs')
         .select()
+        .eq('guild', guildId)
         .eq('channel', channelId)
         .eq('league_id', leagueId)
         .then(dbRes => {
